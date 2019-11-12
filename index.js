c9c364ec64f916a2b1dba57aff8410b848cc8f0d
Travis Geis <ottobonn28@gmail.com>
2019-11-11 16:05:30 -0800

    Bugfixes and useReactive

--- index.js
+++ index.js
@@ -5,16 +5,51 @@ function useAutorun(f) {
   const compContainer = useRef(undefined);
 
   useEffect(() => {
-    if (!compContainer.current) {
-      compContainer.current = Tracker.autorun(computation => {
-        const output = f(computation);
-        setResult(output);
-      });
-    }
-    return () => compContainer.current.stop();
+    // this effect has no body; it is just for the cleanup hook
+    return () => {
+      compContainer.current.stop();
+    };
   }, []);
+
+  if (!compContainer.current) {
+    let firstResult;
+    compContainer.current = Tracker.autorun(computation => {
+      const output = f(computation);
+      if (computation.firstRun) {
+        // On the first run, we want the output synchronously because the
+        // surrounding component is in the process of rendering. setResult will
+        // only make the result availabe on the next render, so we use the
+        // output directly here.
+        firstResult = output;
+      } else {
+        // For every run but the first, we need to trigger the surrounding
+        // component to render again, so we call setResult.
+        setResult(output);
+      }
+    });
+    return firstResult;
+  }
 
   return result;
 }
 
-module.exports = {useAutorun};
+function useReactive(value, equals=EJSON.equals) {
+  const container = useRef(undefined);
+
+  if (!container.current) {
+    const rv = new ReactiveVar(value, equals);
+    container.current = {
+      rv,
+      getter() {
+        // We expose just the get() method of the reactive variable because
+        // setting it would not cause the surrounding component to rerender.
+        return rv.get();
+      }
+    };
+  }
+
+  container.current.rv.set(value);
+  return container.current.getter;
+}
+
+module.exports = {useAutorun, useReactive};
